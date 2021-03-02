f837f0e8468bf1dc604ba3629a262f1137190771
Doddy <yadops@yahoo.fr>
2021-03-02 11:31:44 +0100

    --- Auto-Git Commit ---

--- src/main/java/be/businesstraining/domain/DateTime.java
+++ src/main/java/be/businesstraining/domain/DateTime.java
@@ -5,17 +5,17 @@ import java.util.GregorianCalendar;
 
 public class DateTime {
 
- @SuppressWarnings("unused")
-public int dateTime(){
-  GregorianCalendar time = new GregorianCalendar();
-  int hour = time.get(Calendar.HOUR_OF_DAY);
-  int min = time.get(Calendar.MINUTE);
-  int day = time.get(Calendar.DAY_OF_MONTH);
-  int month = time.get(Calendar.MONTH) + 1;
-  int year = time.get(Calendar.YEAR);
-  
-  System.out.println(hour);
-  return hour;
+  @SuppressWarnings("unused")
+  public int dateTime() {
+    GregorianCalendar time = new GregorianCalendar();
+    int hour = time.get(Calendar.HOUR_OF_DAY);
+    int min = time.get(Calendar.MINUTE);
+    int day = time.get(Calendar.DAY_OF_MONTH);
+    int month = time.get(Calendar.MONTH) + 1;
+    int year = time.get(Calendar.YEAR);
+
+    System.out.println(hour);
+    return hour;
 
   }
 }
